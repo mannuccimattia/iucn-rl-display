47a0c6088f4cc45c89932db2377d8c2171ac82b8
mannuccimattia <mannuccimattia@gmail.com>
2026-02-22 21:13:45 +0100

    [UPD] Minor css fixes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="bg-main-light shadow border-t border-main-dark">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between items-center p-6">
+        <div class="flex justify-between items-center px-6 py-4">
             <div class="shrink-0 flex items-center">
                 <a href="{{ route('dashboard') }}">
-                    <x-application-logo class="block h-9 w-auto fill-currentw" />
+                    <x-application-logo class="block h-9 w-auto fill-current" />
                 </a>
             </div>
             <div class="text-xs text-main-contrast/70 flex flex-col sm:flex-row sm:justify-between gap-x-3 gap-y-1">
